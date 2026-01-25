4bc55666144f0fff746942b710b4d5b97fc963df
shaw820 <shaw.xuan820@gmail.com>
2026-01-25 00:15:52 -0500

    Update WAVWriter.cpp

--- src/utils/WAVWriter.cpp
+++ src/utils/WAVWriter.cpp
@@ -32,7 +32,7 @@ void WAVWriter::setName(String name) {
 }
 
 bool WAVWriter::writeHeader() {
-    file = sd_manager.openFile(_name);
+    file = sd_manager.openFile(_name, true);
 
     if (!file) return false;
 
@@ -44,27 +44,62 @@ bool WAVWriter::writeHeader() {
     return true;
 }
 
+// bool WAVWriter::writeHeaderSizes() {
+//     sd_manager.write_block_at(&file, 40, (byte*)&info.subchunk2Size,4);
+
+//     info.chunkSize = 36 + info.subchunk2Size;
+//     sd_manager.write_block_at(&file, 4, (byte*)&info.chunkSize,4);
+
+//     return true;
+// }
+
 bool WAVWriter::writeHeaderSizes() {
-    sd_manager.write_block_at(&file, 40, (byte*)&info.subchunk2Size,4);
+    // 确保文件是打开的
+    if (!file.isOpen()) {
+        file = sd_manager.openFile(_name, true);
+        if (!file.isOpen()) return false;
+    }
+    
+    sd_manager.write_block_at(&file, 40, (byte*)&info.subchunk2Size, 4);
 
     info.chunkSize = 36 + info.subchunk2Size;
-    sd_manager.write_block_at(&file, 4, (byte*)&info.chunkSize,4);
+    sd_manager.write_block_at(&file, 4, (byte*)&info.chunkSize, 4);
 
     return true;
 }
 
+// bool WAVWriter::writeChunk(uint8_t *block, int size) {
+//     unsigned int written = sd_manager.write_block(&file, block, size);
+//     info.subchunk2Size += written;
+//     return written == size;
+// }
+
 bool WAVWriter::writeChunk(uint8_t *block, int size) {
+    // 确保文件是打开的
+    if (!file.isOpen()) {
+        Serial.println("ERROR: File not open in writeChunk");
+        return false;
+    }
+    
     unsigned int written = sd_manager.write_block(&file, block, size);
+    if (written != size) {
+        Serial.print("WARNING: Written ");
+        Serial.print(written);
+        Serial.print(" bytes, expected ");
+        Serial.println(size);
+    }
     info.subchunk2Size += written;
     return written == size;
 }
 
 bool WAVWriter::startRecording() {
-    file = sd_manager.openFile(_name);
+    file = sd_manager.openFile(_name, true);
 
     info.subchunk2Size = 0;
     info.chunkSize = 36;
-    return file;
+    
+    // return file;
+    return file.isOpen();
 }
 
 bool WAVWriter::endRecording() {
