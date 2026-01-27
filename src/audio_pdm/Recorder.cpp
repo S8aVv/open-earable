2789de6acd68e024a074f34ce19ac86ff85070dd
shaw820 <shaw.xuan820@gmail.com>
2026-01-27 00:45:39 -0500

    Update Recorder.cpp

--- src/audio_pdm/Recorder.cpp
+++ src/audio_pdm/Recorder.cpp
@@ -179,17 +179,19 @@ void Recorder::config_callback(SensorConfigurationPacket *config) {
         }
 
         // file name of the new recording
-        String file_name = "/" + recording_dir + "/Recording_" + String(n) + "_start_" + String(recording_start_timestamp) + ".wav";
+        //String file_name = "/" + recording_dir + "/Recording_" + String(n) + "_start_" + String(recording_start_timestamp) + ".wav";
 
+        // 临时测试：如果文件名变成这个格式，说明代码生效了
+       String file_name = "/" + recording_dir + "/Recording_" + String(n) + "_TEST_START_" + String(recording_start_timestamp) + ".wav";
         // 调试输出
-        Serial.print("DEBUG: Recording file name: ");
-        Serial.println(file_name);
-        Serial.print("DEBUG: Start timestamp: ");
-        Serial.println(recording_start_timestamp);
-        Serial.print("DEBUG: File number: ");
-        Serial.println(n);
-        Serial.print("DEBUG: Current millis: ");
-        Serial.println(millis());
+        // Serial.print("DEBUG: Recording file name: ");
+        // Serial.println(file_name);
+        // Serial.print("DEBUG: Start timestamp: ");
+        // Serial.println(recording_start_timestamp);
+        // Serial.print("DEBUG: File number: ");
+        // Serial.println(n);
+        // Serial.print("DEBUG: Current millis: ");
+        // Serial.println(millis());
 
 
         // set WaveRecorder
